84fb2b19474de378ab85b8c27522f3eb0563a0c5
GoldCastillo <mikko.linna@myy.haaga-helia.fi>
2020-09-10 14:46:04 +0300

    first commit

--- src/main/java/com/example/helloworld/web/FriendController.java
+++ src/main/java/com/example/helloworld/web/FriendController.java
@@ -1,38 +1,34 @@
 package com.example.helloworld.web;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.helloworld.domain.Friend;
 
-
-
 @Controller
 public class FriendController {
-	List<Friend> friends = new ArrayList<Friend>();
-	
-	@RequestMapping(value="/friend", method=RequestMethod.GET)
-	public String friendList(Model model) {
-		model.addAttribute("friends", friends);
-		return "friend";
-		
-		
-	}
-	@RequestMapping(value="/friend", method=RequestMethod.POST)
-	public String friendlistSubmit(@ModelAttribute Friend friend) {
-		friends.add(friend);
-		return "redirect:/friend";
-	}
-	}
-	
-	
 
+	private List<Friend> friendList = new ArrayList<Friend>();
+	
+	@GetMapping("/home")
+	public String friendForm(Model model) {
+		model.addAttribute("friend", new Friend());
+		model.addAttribute("friendList", friendList);
+		return "index";
+	}
+	
+	@GetMapping("/index")
+	public String friendList(@ModelAttribute Friend friend, Model model) {
+		friendList.add(friend);
+		model.addAttribute("friend", new Friend());
+		model.addAttribute("friendList", friendList);
+		return "index";
+	}
+	
+	
+}
